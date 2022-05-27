cb93484bf11ce3d89d1191d60680af072f0ea177
Maxwb7 <Maxwb7@gmail.com>
2022-05-27 14:58:32 -0700

    Add files via upload

---
+++ #Save Back up file tool.py
@@ -0,0 +1,3 @@
+#Save Back up file tool 
+# to-do: build program, build gui. Release code / program to community for Elden Ring
+#:\Users\[username]\AppData\Roaming\EldenRing - where save data is located .sl2 file is file type
